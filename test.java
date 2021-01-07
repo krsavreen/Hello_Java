18c2cb280558b857b3b8c2adbffda1fbad58b3f1
Savvy <krsavreen@gmail.com>
2021-01-07 16:51:18 -0700

    Test file added

---
+++ test.java
@@ -0,0 +1,19 @@
+package firstjavaproj;
+
+public class test 
+{
+
+	public static void main(String[] args) 
+	{		
+		System.out.println("Hello World!");
+        
+		greet();
+        
+		System.out.println("End of main()");
+	}
+	
+	public static void greet()
+	{
+     System.out.println("Hi! How are you?");
+	}
+}
